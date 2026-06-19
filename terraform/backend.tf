6bc49b3d330e08823b4775fe592404b85f1812e2
omar1111eamsa <oserghini090@gmail.com>
2026-06-19 02:02:35 +0100

    security: redact AWS account id from tfstate bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-airbnb-terraform-state"
+    bucket = "my-airbnb-tfstate-CHANGE_ME"
     key = "infra/terraform.tfstate"
     region = "eu-west-1"
     encrypt = true
